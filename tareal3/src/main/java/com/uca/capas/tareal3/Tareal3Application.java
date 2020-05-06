0e1d85771f452bbdc331dca96a7246847beaf374
Elsy Chavez <00125717@uca.edu.sv>
2020-05-05 22:07:40 -0600

    primer commit tarea

---
+++ tareal3/src/main/java/com/uca/capas/tareal3/Tareal3Application.java
@@ -0,0 +1,13 @@
+package com.uca.capas.tareal3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tareal3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tareal3Application.class, args);
+	}
+
+}
---
+++ tareal3/src/test/java/com/uca/capas/tareal3/Tareal3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas.tareal3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tareal3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
